324041070240769225ec224b448bf92de5e992d2
lencinasalejo <lencinasalejo@gmail.com>
2025-05-28 20:21:28 -0300

    number spiral enhanced

--- number_spiral/number_spiral.cpp
+++ number_spiral/number_spiral.cpp
@@ -4,16 +4,8 @@ using namespace std;
 
 int main()
 {
-    vector<vector<long long>> default_matrix = {
-        {0, 0, 0, 0 , 0, 0, 0, 0},
-        {0, 1, 2, 9, 10, 25, 26, 49},
-        {0, 4, 3, 8, 11, 24, 27, 48},
-        {0, 5, 6, 7, 12, 23, 28, 47},
-        {0, 16, 15, 14, 13, 22, 29, 46},
-        {0, 17, 18, 19, 20, 21, 30, 45},
-        {0, 36, 35, 34, 33, 32, 31, 44},
-        {0, 37, 38, 39, 40, 41, 42, 43}};
-
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
     int x, y;
@@ -21,50 +13,47 @@ int main()
     vector<pair<int, int>> coordinates;
     for(int i = 0; i < t; i++)
     {
-        cin >> x >> y;
-        coordinates.push_back(make_pair(x, y));
+        cin >> y >> x;
+        coordinates.push_back(make_pair(y, x));
     }
     for(int i = 0; i < t; i++)
     {
-        x = coordinates[i].first;
-        y = coordinates[i].second;
-        if(x <= 7 && y <= 7)
+        y = coordinates[i].first;
+        x = coordinates[i].second;
+        if(x == 1 && y == 1)
         {
-            cout << default_matrix[x][y] << "\n";
+            number_result = 1;
         } else
         {
             long long square_num = max(x, y);
-            if((square_num % 2) == 0)
+            long long diag_number = ((square_num * square_num) + ((square_num - 1) * (square_num - 1) + 1)) / 2;
+            number_result = diag_number;
+            if(y > x)
             {
-                square_num++;
-            }
-            long long max_square_value = square_num * square_num;
-            long long min_square_value = (square_num - 2) * (square_num - 2) + 1;
-
-            if( (x == 1) && (y == (square_num - 1)) )
-            {
-                //min value case
-                number_result = min_square_value;
-            } else if( (x == 1) && (y == square_num) )
-            {
-                //max value case
-                number_result = max_square_value;
-            } else if( (y == (square_num - 1)) && (x < square_num) )
-            {
-                number_result = min_square_value + (x - 1);
-            } else if( (y < (square_num - 1)) && (x < square_num) )
-            {
-                number_result = min_square_value + (x - 1) + (square_num - 1 - y);
-            } else if(y == square_num)
-            {
-                number_result = max_square_value - (x - 1);
+                //me muevo horizontalmente
+                if((square_num % 2) == 0)
+                {
+                    number_result = diag_number + (square_num - x);
+                } else
+                {
+                    number_result = diag_number - (square_num - x);
+                }
             } else
             {
-                number_result = max_square_value - (square_num - 1) - (square_num - y);
+                if(y < x)
+                {
+                    //me muevo verticalmente
+                    if((square_num % 2) == 0)
+                    {
+                        number_result = diag_number - (square_num - y);
+                    } else
+                    {
+                        number_result = diag_number + (square_num - y);
+                    }
+                }
             }
-
-            cout << number_result << "\n";
         }
+        cout << number_result << "\n";
     }
 
     return 0;
