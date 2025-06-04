fea1ee00ffb256765f083fb9da4bcdc6a688300b
lencinasalejo <lencinasalejo@gmail.com>
2025-06-03 22:42:47 -0300

    two knights enhanced

--- two_knights/two_knights.cpp
+++ two_knights/two_knights.cpp
@@ -14,26 +14,15 @@ int main()
         long long result = 0;
         long long table_size = k * k;
         long long full_combinations;
-//        full_combinations = (factorial(table_size)) / (2 * (factorial(table_size - 2)));
         full_combinations = (table_size * (table_size - 1)) / 2;
-//        cout << "full combinations: " << full_combinations << "\n";
-        if(k > 2)
+        if(k > 1)
         {
             long long res_movements = (k - 2) * (10 + (4 * (k - 4)));
             res_movements += 4 + ((k - 4) * 2);
             result = full_combinations - res_movements;
-        } else
-        {
-            if(k < 2)
-            {
-                result = 0;
-            } else
-            {
-                result = full_combinations;
-            }
         }
         cout << result << "\n";
     }
-//    cout << "Hello world!" << endl;
+
     return 0;
 }
